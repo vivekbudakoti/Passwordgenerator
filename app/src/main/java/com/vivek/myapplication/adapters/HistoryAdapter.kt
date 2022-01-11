7eb77b876e6d71b3b2e37974b394e6744f92bb0d
Vivek <thevivekbudakoti@gmail.com>
2022-01-11 18:34:23 +0530

    Adapter with dummy data for the history has been seted completely

--- app/src/main/java/com/vivek/myapplication/adapters/HistoryAdapter.kt
+++ app/src/main/java/com/vivek/myapplication/adapters/HistoryAdapter.kt
@@ -1,9 +1,16 @@
 package com.vivek.myapplication.adapters
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.RecyclerView
 import com.vivek.myapplication.R
 
@@ -11,17 +18,33 @@ import com.vivek.myapplication.model.Hist
 import java.util.ArrayList
 
 class HistoryAdapter(val context: Context, val itemList: ArrayList<Hist>) : RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder>() {
+    lateinit var myClipboard : ClipboardManager
+    lateinit var myClip: ClipData
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_history_single, parent, false)
+        myClipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
 
         return HistoryViewHolder(view)
     }
 
     class HistoryViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val edt_history_pass:EditText = view.findViewById(R.id.edt_history)
+        val img_copy : ImageView = view.findViewById(R.id.img_hist_copy)
+        val img_delete : ImageView = view.findViewById(R.id.img_hist_delete)
 
     }
 
     override fun onBindViewHolder(holder: HistoryAdapter.HistoryViewHolder, position: Int) {
+        val item = itemList[position]
+        holder.edt_history_pass.setText(item.hist_pass,TextView.BufferType.EDITABLE)
+        holder.img_copy.setOnClickListener {
+            myClip =ClipData.newPlainText("text",item.hist_pass)
+            myClipboard.setPrimaryClip(myClip)
+
+            Toast.makeText(context,"Password: \"${holder.edt_history_pass.text.toString()}\"\ncopied successfully",
+                Toast.LENGTH_SHORT).show()
+        }
 
     }
 
--- app/src/main/java/com/vivek/myapplication/fragments/history_fragment.kt
+++ app/src/main/java/com/vivek/myapplication/fragments/history_fragment.kt
@@ -1,13 +1,25 @@
 package com.vivek.myapplication.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.vivek.myapplication.R
+import com.vivek.myapplication.adapters.HistoryAdapter
+import com.vivek.myapplication.model.Hist
 
 class history_fragment : Fragment() {
+
+    lateinit var recyclerHistory : RecyclerView
+    lateinit var layoutManager: RecyclerView.LayoutManager
+    lateinit var recyclerAdapter : HistoryAdapter
+
+    val HistInfoList = arrayListOf<Hist>()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,6 +27,21 @@ class history_fragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
      var view = inflater.inflate(R.layout.fragment_history, container, false)
+
+        recyclerHistory = view.findViewById(R.id.recyclerHistory)
+        layoutManager = LinearLayoutManager(activity)
+
+        HistInfoList.add(Hist("vi79xh]089h9"));
+        HistInfoList.add(Hist("xni8910{];'279dho"));
+        HistInfoList.add(Hist("h80shjo["));
+        HistInfoList.add(Hist("0shjo["));
+        HistInfoList.add(Hist("bupo=270';"));
+
+        recyclerAdapter = HistoryAdapter(activity as Context, HistInfoList)
+
+        recyclerHistory.adapter = recyclerAdapter
+        recyclerHistory.layoutManager = layoutManager
+
                 return view
     }
 
