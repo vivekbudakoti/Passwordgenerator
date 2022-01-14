a12c0ca03f19c6136d9c4f739dc16df8d85cbcf8
Vivek <thevivekbudakoti@gmail.com>
2022-01-14 19:29:14 +0530

    add button added in save screen

--- app/src/main/java/com/vivek/myapplication/fragments/save_fragment.kt
+++ app/src/main/java/com/vivek/myapplication/fragments/save_fragment.kt
@@ -1,14 +1,19 @@
 package com.vivek.myapplication.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.vivek.myapplication.R
 
 
 class save_fragment : Fragment() {
+
+    lateinit var floatingAddButton : FloatingActionButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +21,13 @@ class save_fragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         var view = inflater.inflate(R.layout.fragment_save, container, false)
+
+        floatingAddButton = view.findViewById(R.id.floating_button)
+
+        floatingAddButton.setOnClickListener {
+            Toast.makeText(activity as Context,"Floating pressed",Toast.LENGTH_SHORT).show()
+        }
+
         return view
     }
 }
